80b5efdaff0639dd9fd01500435e851bb42452e5
taznimt <taznimanwar06@gmail.com>
2024-06-13 15:39:14 +0700

    member methode prosedure insert

--- binaresearchtree/binaresearchtree.cpp
+++ binaresearchtree/binaresearchtree.cpp
@@ -27,6 +27,32 @@ public:
     {
         ROOT = nullptr;
     }
+    void insert(string element) //Insert a node in the search tree
+    {
+        node* newnode = new node(element, nullptr, nullptr); // Allocate memory for the new node
+        newnode->info = element; // Assign value to the data field of the new node
+        newnode->leftchild = nullptr; // Make left child of the new node point to NULL
+        newnode->rightchild = nullptr; // Make right child of the new node point to NULL
+
+        node* parent = nullptr;
+        node* currentnode = nullptr;
+        search(element, parent, currentnode); // Locate teh node which will be the parent of the node to be insert
+
+        if (parent == nullptr) // If the parent is NULL (Tree is empty)
+        {
+            ROOT = newnode; // Mark the new node as ROOT
+            return; //Exit
+        }
+
+        if (element < parent->info) // If the value in the data field of the new node is less than that of the parent
+        {
+            parent->leftchild = newnode; // Make the left child of the parent point to the new node
+        }
+        else if (element > parent->info) // If the value in the data field of the new node is greater than that of the parent
+        {
+            parent->rightchild = newnode; //  Make the right child of the parent point to the new node
+        }
+    }
 };
 
 int main()
